793f3025be2448f5e283f91adb2dcd75fe06b07b
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-30 18:13:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber (array,Name) {
+  array.push(Name)
+  return `Welcome, ${Name}. You are number ${array.length} in line.`
+}
+function nowServing (array) {
+  if (array.length > 0) {
+    return `Currently serving ${array.shift()}.`
+}
+else {
+  return "There is nobody waiting to be served!"
+}
+}
+function currentLine (array) {
+  if (array.length > 0) { 
+  for (let i = 0; i === array.length; i++) {
+  return `The line is currently: ${i + 1}. ${array[i]}`;
+  } 
+  } else {
+    return "The line is currently empty.";
+  }
+  }
